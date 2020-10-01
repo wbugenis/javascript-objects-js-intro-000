325af93a21263e7e3ac2947bb86781e03b5332ff
gorgeous-skill-0664 <wbugenis@gmail.com>
2020-10-01 01:58:29 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,9 +3,9 @@ var playlist = { 'Mr Zim': 'Plamp' };
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle;
   return playlist;
-};
+}
 
 function removeFromPlaylist(playlist, artistName) {
   playlist.remove(artistName);
   return playlist;
-};
+}
